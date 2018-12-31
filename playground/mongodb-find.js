6fc0000a2352f4ec975bb3624973a1f435adb396
Daniel <danielabrgel91@gmail.com>
2018-12-31 20:19:38 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const MongoClinect = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+MongoClinect.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
+    if (err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    client.collection('Users').find({name:'Daniel'}).toArray().then((docs)=> {
+        console.log('Todos');
+        console.log(JSON.stringify(docs,undefined,2));
+    },(err) =>{
+        console.log('Unable to fetch todos',err);
+    });
+
+
+    // client.collection('Todos').find().count().then((count)=> {
+    //     console.log(`Todos count: ${count}`);
+    //     console.log(JSON.stringify(docs,undefined,2));
+    // },(err) =>{
+    //     console.log('Unable to fetch todos',err);
+    // });
+    //
+
+    //client.close();
+} );
